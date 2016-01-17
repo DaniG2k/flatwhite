f2a62355557efc17c99c8cc7d371a55d84df4df4
DaniG2k <byakugan.87@gmail.com>
2016-01-17 23:56:28 +0000

    Added ckeditor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	validates_presence_of :title, :body
-	default_scope {order(:created_at => :desc)}
+  validates_presence_of :title, :body
+  default_scope {order(:created_at => :desc)}
 end
