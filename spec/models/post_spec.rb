7f21784ca5b5dc96730958a0e1fc1b30492e2be4
Daniele Pestilli <dpestilli@funnelback.com>
2016-01-18 09:25:36 +0000

    Added first Rspec tests

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Post, :type => :model do
+  describe 'validations' do
+    before :all do
+      @post1 = Post.create(
+        :title => 'Some interesting title',
+        :body => 'Some interesting body',
+      )
+      @post2 = @post1.dup
+    end
+    after :all do
+      expect(@post2).not_to be_valid
+    end
+
+    it 'requires a title' do
+      @post2.title = ''
+    end
+    it 'requires a body' do
+      @post2.body = ''
+    end
+  end
+end
