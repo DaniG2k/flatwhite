8f08c009ca2ceb01b55d5d84fce6c4e337ce8800
Daniele Pestilli <dpestilli@funnelback.com>
2016-01-18 14:43:17 +0000

    Added preview helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def preview(post)
+    strip_tags(post.body)
+    .truncate(300, :separator => ' ')
+    .gsub(/(\w)\.(\w)/, '\1. \2')
+    .gsub(/&ldquo;|&rdquo;/, '"')
+  end
 end
