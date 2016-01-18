12726366e547c660ed9109468d8415d5860287a7
Daniele Pestilli <dpestilli@funnelback.com>
2016-01-18 14:28:34 +0000

    Added copyright_year helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,13 @@ module ApplicationHelper
   def title(*parts)
     content_for(:title) { (parts << t(:website)).join(' - ') } if parts.present?
   end
+  
   def set_html_lang
     I18n.locale.to_s
+  end
+
+  def copyright_date
+    year = Time.zone.now.year
+    year.to_i == 2016 ? "#{year}" : "2016-#{year}"
   end
 end
