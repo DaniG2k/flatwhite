90017c1d3dc60a1eee1096cc1e3911edcd42e2c6
Daniele Pestilli <dpestilli@funnelback.com>
2016-01-21 16:00:55 +0000

    Added lead image with caption

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates_presence_of :title, :body
   validates :title, uniqueness: true
   default_scope {order(:created_at => :desc)}
+  mount_uploader :post_image, PostImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118104538) do
+ActiveRecord::Schema.define(version: 20160121155826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160118104538) do
     t.datetime "updated_at", null: false
     t.string   "title"
     t.text     "body"
+    t.string   "post_image"
+    t.string   "caption"
   end
 
   create_table "users", force: :cascade do |t|
